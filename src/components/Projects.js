fb8c7b7c6dba98a3aba7e60940acab1ab9051b96
alonso147411 <alonso.147411@e.unavarra.es>
2025-04-05 22:14:04 +0200

    [#] - Actualizacion de los proyectos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,14 +12,19 @@ return (
                 <a href="https://github.com/alonso147411/Password_gestor" target = "_blanck" className="btnVerMas">Ver más</a>
             </div>
             <div className="project-card" data-aos="zoom-in" data-aos-delay="200">
-                <h3>Proyecto 2</h3>
-                <p>Descripción breve del proyecto.</p>
-                <a href="https://github.com" target = "_blanck"  className="btnVerMas">Ver más</a>
+                <h3>VitaBite</h3>
+                <p>Aplicación de Nutrición y Entrenamiento Personalizado</p>
+                <a href="https://github.com/alonso147411/VitaBite" target = "_blanck"  className="btnVerMas">Ver más</a>
             </div>
             <div className="project-card" data-aos="zoom-in" data-aos-delay="400">
-                <h3>Proyecto 3</h3>
-                <p>Descripción breve del proyecto.</p>
-                <a href="https://github.com"  target = "_blanck" className="btnVerMas">Ver más</a>
+                <h3>Sentia</h3>
+                <p>Una aplicación web diseñada para mejorar la comunicación y preservar la salud mental entre parejas</p>
+                <a href="https://github.com/alonso147411/Sentia"  target = "_blanck" className="btnVerMas">Ver más</a>
+            </div>
+            <div className="project-card" data-aos="zoom-in" data-aos-delay="600">
+                <h3>Katas</h3>
+                <p>Katas hechas en clase de la universidad</p>
+                <a href="https://github.com/alonso147411?tab=repositories&q=&type=public&language=&sort=" target = "_blanck" className="btnVerMas">Ver más</a>
             </div>
         </div>
     </section>
